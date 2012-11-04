f908096965181331fbb5619225c39032a59e641b
Zhifeng Gu <guzhifeng1979@hotmail.com>
2012-11-04 23:36:12 +0800

    clean up files

--- misc/coldplug.sh
+++
@@ -1,7 +0,0 @@
-coldplug_interface_pppoe() {
-    local config="$1"
-    config_get ifname "$config" ifname
-
-    [ -n "$ifname" ] && grep "^$ifname:" /proc/net/dev &> /dev/null && \
-        setup_interface "$ifname" "$config"
-}
